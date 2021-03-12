cb44b15058d0c5ef957f9be49e8ae971a32c7d8c
david-monroy <djmonroy.17@est.ucab.edu.ve>
2021-03-12 15:31:22 -0400

    Stepper proyecto, payload listo

--- controlca-client/src/views/projects/ProjectAdd.vue
+++ controlca-client/src/views/projects/ProjectAdd.vue
@@ -20,7 +20,9 @@
 
       <v-divider></v-divider>
 
-      <v-stepper-step step="3">
+      <v-stepper-step
+        :complete="formStep > 3"
+        step="3">
         Colaboradores
       </v-stepper-step>
 
@@ -153,10 +155,7 @@
         </v-expansion-panel-content>
         </v-expansion-panel>
         </v-expansion-panels>
-            
-
-                
-        
+           
         <div style="display: flex; justify-content: space-between">
             <v-btn text
             >
@@ -176,11 +175,67 @@
       </v-stepper-content>
 
       <v-stepper-content step="3">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
+        <div v-if="projectData.workers.length>0">
+            <v-simple-table max-height="240px" >
+                <template v-slot:default>
+                    <thead >
+                            <tr>
+                            <th class="text-center">
+                                Nro.
+                            </th>
+                            <th class="text-center">
+                                Nombre
+                            </th>
+                            <th class="text-center">
+                                Apellido
+                            </th>
+                            <th class="text-center">
+                                Rol
+                            </th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                        v-for="(worker,w) in projectData.workers"
+                        :key="w"
+                        >
+                        <td class="text-center">{{ w+1 }}</td>
+                        <td class="text-center">{{ worker.name }}</td>
+                        <td class="text-center">{{ worker.lastname }}</td>
+                        <td class="text-center">{{ worker.estimated_hours }}</td>
+                        </tr>
+                    </tbody>
+                    </template>
+                </v-simple-table>
+        </div>
+        <v-expansion-panels focusable class="px-5 mt-2 mb-5">
+            <v-expansion-panel>
+                <v-expansion-panel-header>Añadir colaborador</v-expansion-panel-header>
+                <v-expansion-panel-content class="py-4">
+            <v-row class="pa-0 ma-0 form-row-rol">
+            <v-select
+                v-model="temp_worker_id"
+                :items="origin_workers"
+                label="Colaborador"
+                item-text="email"
+                item-value="id"
+                dense
+                required
+              ></v-select>
+        </v-row>
+        <v-row class="pa-0 ma-0 mb-0 form-row-rol">
+            <v-text-field
+                v-model="temp_worker_rol_in_project"
+                label="Rol"
+                type="text"
+            ></v-text-field>
+        </v-row>
+        <v-btn class="simple-btn mt-2 mx-auto btn-block" @click="addWorkerToProject()">
+          Añadir colaborador al proyecto
+        </v-btn>
+        </v-expansion-panel-content>
+        </v-expansion-panel>
+        </v-expansion-panels>
 
         <div style="display: flex; justify-content: space-between">
             <v-btn text>
@@ -192,18 +247,106 @@
             </v-btn>
             <v-btn
                 color="primary"
-                @click="formStep = 3"
+                @click="formStep = 4"
                 >
                 Siguiente
                 </v-btn>
         </div>
       </v-stepper-content>
       <v-stepper-content step="4">
-        <v-card
-          class="mb-12"
-          color="grey lighten-1"
-          height="200px"
-        ></v-card>
+
+        <div class="project-confirm-section pa-4 body-2 mb-4">
+            <div style="display: flex; justify-content: space-between;">
+                <div>
+                    <strong>Código: </strong> <p>{{projectData.code}}</p>
+                </div>
+                <div>
+                    <strong>Área: </strong> <p>{{projectData.area}}</p>
+                </div>
+                <div>
+                    <strong>Líder: </strong> <p>{{currentUser.name}} {{currentUser.lastname}}</p>
+                </div>
+            </div>
+            
+            <div class="mb-2 pb-0 body-2">
+                <p><strong>Descripción: </strong>{{projectData.description}}</p> 
+            </div>
+        </div>
+
+        <v-expansion-panels focusable class="pa-0 mb-2">
+            <v-expansion-panel>
+                <v-expansion-panel-header>Productos ({{projectData.products.length}})</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <v-simple-table max-height="240px" >
+                        <template v-slot:default>
+                        <thead >
+                            <tr>
+                            <th class="text-center">
+                                Nro.
+                            </th>
+                            <th class="text-center">
+                                Producto
+                            </th>
+                            <th class="text-center">
+                                Horas estimadas
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="(product,p) in products"
+                            :key="p"
+                            >
+                            <td class="text-center">{{ p+1 }}</td>
+                            <td class="text-center">{{ product.name }}</td>
+                            <td class="text-center">{{ product.estimated_hours }}</td>
+                            </tr>
+                        </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+
+        <v-expansion-panels focusable class="pa-0 mb-5">
+            <v-expansion-panel>
+                <v-expansion-panel-header>Colaboradores ({{projectData.workers.length}})</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <v-simple-table max-height="240px" >
+                        <template v-slot:default>
+                        <thead >
+                            <tr>
+                            <th class="text-center">
+                                Nro.
+                            </th>
+                            <th class="text-center">
+                                Nombre
+                            </th>
+                            <th class="text-center">
+                                Apellido
+                            </th>
+                            <th class="text-center">
+                                Rol
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr
+                            v-for="(worker,w) in projectData.workers"
+                            :key="w"
+                            >
+                            <td class="text-center">{{ w+1 }}</td>
+                            <td class="text-center">{{ worker.name }}</td>
+                            <td class="text-center">{{ worker.lastname }}</td>
+                            <td class="text-center">{{ worker.rol_in_project }}</td>
+                            </tr>
+                        </tbody>
+                        </template>
+                    </v-simple-table>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+        
 
         <div style="display: flex; justify-content: space-between">
             <v-btn text>
@@ -229,6 +372,7 @@
 
 <script>
 import ProductDataService from "../../services/ProductDataService";
+import UserDataService from "../../services/UserDataService";
 export default {
   name: "projects-add",
   data: () => ({
@@ -240,14 +384,15 @@ export default {
           area: "",
           leader: null,
           products: [],
-          workers: [{
-              id: null,
-              rol_in_project: null,
-          }]
+          workers: [],
       },
       origin_products: [],
       temp_product_id: null,
       temp_product_estimated_hours: null,
+
+      origin_workers: [],
+      temp_worker_id: null,
+      temp_worker_rol_in_project: null,
   }),
   computed: {
     currentUser() {      
@@ -281,10 +426,43 @@ export default {
             });
         } 
         
+    },
+    retrieveUsers() {
+      UserDataService.getAll()
+        .then((response) => {
+          this.origin_workers = response.data;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    addWorkerToProject(){
+        let worker_name = "";
+        let worker_lastname = "";
+        let itExists = false;
+        this.projectData.workers.forEach(worker => {
+            if (worker.id == this.temp_worker_id) itExists = true;
+        });
+        if (!itExists) {
+            this.origin_workers.forEach(op => {
+                if (op.id==this.temp_worker_id) {
+                    worker_name = op.name;
+                    worker_lastname = op.lastname;
+                } 
+            });
+            this.projectData.workers.push({
+                id: this.temp_worker_id, 
+                estimated_hours: this.temp_worker_rol_in_project,
+                name: worker_name,
+                lastname: worker_lastname,
+            });
+        } 
+        
     }
   },
   mounted() {
       this.retrieveProducts();
+      this.retrieveUsers();
   }
 }
 </script>
@@ -298,5 +476,10 @@ export default {
         border-radius: 20px;
         padding-top: 10px;
         margin-bottom: 10px;
+    }
+    .project-confirm-section{
+        border: 1px solid lightgray;
+        border-radius: 10px;
+        padding: 8px;
     }
 </style>
